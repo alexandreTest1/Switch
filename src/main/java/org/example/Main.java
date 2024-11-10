4de1a97e81c274dba2705cdfc8d299b2a4aee5b2
alexandreTest1 <alekotevzadze1988@gmail.com>
2024-11-11 00:10:21 +0400

    Switch/Case

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import javax.imageio.ImageTranscoder;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int finalScore = 92;
+
+        switch (finalScore){
+            case 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100 :
+                System.out.println("A");
+                break;
+
+            case 80, 81, 82, 83, 84, 85, 86, 87, 88, 89 :
+                System.out.println("B");
+                break;
+
+            case 70, 71, 72, 73, 74, 75, 76, 77, 78, 79 :
+                System.out.println("C");
+                break;
+
+            default:
+                System.out.println("არ გეკუთვნის სერტიფიკატი");
+        }
+
+    }
+
+}
